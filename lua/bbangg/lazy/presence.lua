500c3d9f1487187d5b6877db8843eecc5f18d2e7
bbangg <bbanggdev@gmail.com>
2024-09-01 18:48:36 +0300

    chore: remove comments

--- lua/bbangg/lazy/presence.lua
+++ lua/bbangg/lazy/presence.lua
@@ -5,18 +5,17 @@ return {
             require("presence").setup({
                 -- General options
                 auto_update         = true,
-                neovim_image_text   = "neovim?",
+                neovim_image_text   = "neovim",
                 main_image          = "file",
                 client_id           = "793271441293967371",
                 log_level           = nil,
-                debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-                enable_line_number  = true,                      -- Displays the current line number instead of the current project
-                blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-                buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-                file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-                show_time           = true,                       -- Show the timer
+                debounce_timeout    = 10,
+                enable_line_number  = true,
+                blacklist           = {},
+                buttons             = true,
+                file_assets         = {},
+                show_time           = true,
 
-                -- Rich Presence text options
                 editing_text        = function (filename)
                     local name = filename:match("^.+/(.+)$") or filename
                     local base, ext = name:match("^(.-)(%.[^.]+)$")
@@ -30,8 +29,8 @@ return {
                 git_commit_text     = "Committing changes",
                 plugin_manager_text = "Managing plugins",
                 reading_text        = "Reading ***",
-                workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-                line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+                workspace_text      = "Working on %s",
+                line_number_text    = "Line %s out of %s",
             })
         end
     }
