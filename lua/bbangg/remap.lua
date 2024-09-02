e7903e6603391daf05e528d8fea4627efce02b93
bbangg <bbanggdev@gmail.com>
2024-09-02 13:21:38 +0300

    feat: lsp format keybinding

--- lua/bbangg/remap.lua
+++ lua/bbangg/remap.lua
@@ -21,5 +21,6 @@ vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
