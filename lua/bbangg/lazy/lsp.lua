63dc8d56a78dcbdba3c5faa1e504bba97347ae1c
Samet B. <73590558+bbangg@users.noreply.github.com>
2024-12-31 13:29:13 -0800

    Update lsp.lua

--- lua/bbangg/lazy/lsp.lua
+++ lua/bbangg/lazy/lsp.lua
@@ -26,7 +26,7 @@ return {
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
-        "tsserver",
+        "ts_ls",
         "lua_ls",
         "rust_analyzer",
         "svelte",
