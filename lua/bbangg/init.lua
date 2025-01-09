b42c26555091ec7da82b6b9a7995794fb4fdd3c2
bbangg <bbanggdev@gmail.com>
2025-01-09 15:20:46 +0300

    automated dev commit

--- lua/bbangg/init.lua
+++ lua/bbangg/init.lua
@@ -1,4 +1,3 @@
-require("bbangg.set")
 require("bbangg.remap")
 require("bbangg.lazy_init")
 
@@ -8,15 +7,16 @@ local bbanggGroup = augroup('bbangg', {})
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd('LspAttach', {
-    group = bbanggGroup,
-    callback = function (e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set('n', 'gd', function () vim.lsp.buf.definition() end, opts)
-        vim.keymap.set('n', 'K', function () vim.lsp.buf.hover() end, opts)
-        vim.keymap.set('n', '<leader>vca', function () vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set('n', '<leader>vrr', function () vim.lsp.buf.references() end, opts)
-        vim.keymap.set('n', '<leader>vrn', function () vim.lsp.buf.rename() end, opts)
-    end
+  group = bbanggGroup,
+  callback = function(e)
+    require("bbangg.set")
+    local opts = { buffer = e.buf }
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+  end
 })
 
 vim.g.netrw_browser_split = 0
